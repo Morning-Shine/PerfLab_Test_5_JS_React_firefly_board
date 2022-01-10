f9376f1b2bf2fdcaa0ab62b18f40793169b15a40
Morning-Shine <maskacat@gmail.com>
2022-01-10 17:59:46 +0300

    got user params & set it to redux

--- src/index.js
+++ src/index.js
@@ -1,45 +1,19 @@
-import React, { createContext } from "react";
+import React from "react";
+import "./index.css";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-// import firebase from "firebase/app";
-// import "firebase/auth";
-// import "firebase/firestore";
-// https://firebase.google.com/docs/auth/web/start почитать! 9 версия отличается от видео
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-// const app = initializeApp({
-//   apiKey: "AIzaSyBVfDFTI6cEyHwpPCxwRVT40ZFVTZyIhRc",
-//   authDomain: "taskboard-e1c00.firebaseapp.com",
-//   projectId: "taskboard-e1c00",
-//   storageBucket: "taskboard-e1c00.appspot.com",
-//   messagingSenderId: "483191536510",
-//   appId: "1:483191536510:web:f10f8c5090124332a5512f",
-//   measurementId: "G-7DPZH8S6XH",
-// });
-
-// export const Context = createContext(null);
-
-// const auth = firebase.auth();
-// const firestore = firebase.firestore();
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <Context.Provider value={{
-        firebase,
-        auth,
-        firestore
-      }}> */}
-        <App />
-      {/* </Context.Provider> */}
+        <Provider store={store}>
+          <App />
+        </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
