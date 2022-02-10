057255eb4139a202da7c83717ba6544e1baaf0a9
Morning-Shine <maskacat@gmail.com>
2022-02-10 22:56:55 +0300

    v 1.0

--- src/components/TableWrapper.js
+++ src/components/TableWrapper.js
@@ -4,19 +4,18 @@ import TableBasic from "./TableBasic";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchTickets } from "../async/fetchTickets";
 
-
-
 //TODO сколько получаем? Зависит от отображения?
 
 export default function TableWrapper() {
-  const [tickets, setTickets] = useState([]);//onSnapshot
+  const [tickets, setTickets] = useState([]); //onSnapshot
 
-  const viewProp = useSelector(state => state.tableView.currentView);
+  const viewProp = useSelector((state) => state.tableView.currentView);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    fetchTickets(setTickets, 8);//onSnapshot
+    fetchTickets(setTickets, 8); //onSnapshot
     //dispatch(fetchTickets({ startAt: 1, limit: 8 }));//через get с limit()
+    //TODO return на отписку! 
   });
 
   let render;
@@ -27,9 +26,5 @@ export default function TableWrapper() {
 }
 
 const DivCont = styled.div`
-  /* border: 1px solid red; */
-  //width: 100px;
-  /* grid-row-start: 2;
-  grid-row-end: 3; */
   margin-bottom: 10px;
 `;
